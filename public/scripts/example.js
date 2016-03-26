cf42a57f4476cfa9408937cd267ab5ccd615fec3
maimai <maimai77@agileware.jp>
2016-03-26 10:18:14 +0900

    add form to CommentForm

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -66,11 +66,25 @@ var Comment = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = ReactDOM.findDOMNode(this.refs.author).value.trim();
+    var text = ReactDOM.findDOMNode(this.refs.text).value.trim();
+    if (!text || !author) {
+      return;
+    }
+    // TODO: サーバにリクエストを送信
+    ReactDOM.findDOMNode(this.refs.author).value = '';
+    ReactDOM.findDOMNode(this.refs.text).value = '';
+    return;
+  },
   render: function() {
     return (
-      <div clssName="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form clssName="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something..." ref="text" />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 });
